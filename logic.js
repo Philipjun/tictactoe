4ab7b7845b39c3be218eba79e374b734719672f6
Philip Jun <inbox@philipjun.com>
2014-04-25 15:28:03 -0400

    add hover

--- logic.js
+++ logic.js
@@ -1,3 +1,30 @@
-$document.ready(){
-  
+$document.ready(function changeTurn(){
+  if(nextTurn == 'X'){
+    nextTurn = 'O';
+  } else {
+    nextTurn = 'X';
+  }
+
+});
+
+
+var nextTurn ='X'
+
+if (this.id =="c1")
+{
+  if document.getElementById("c1").innerHTML == ""){
+    document.getElementById("c1").innerHTML == nextTurn;
+      changeTurn();
+  }
+
 }
+
+function changeTurn(){
+  if(nextTurn == 'X'){
+    nextTurn = 'O';
+  } else {
+    nextTurn = 'X';
+  }
+
+}
+
